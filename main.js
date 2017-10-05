01294abd4eca29733469aa2802db8840d4754400
tahbristol <tahbristol@gmail.com>
2017-10-05 12:52:34 -0400

    this lab

---
+++ main.js
@@ -0,0 +1,78 @@
+var cake = {
+  name: "German Chocolate Cake",
+  ingredients: ["eggs", "flour", "oil", "chocolate", "sugar", "butter"],
+  topping: "coconut frosting",
+  bakeTemp: "425 degrees",
+  bakeTime: "45 minutes",
+  customer: "Tommy",
+  decorate: function(updateFunction) {
+    //console.log(this); this is cake
+    var status = "Decorating with " + this.topping + ". Ready to eat soon!"
+    updateFunction(status)
+    setTimeout(() => {
+  //console.log(this); this is cakef
+    var s = serve("Happy Eating!", this.customer).bind(cake);
+    console.log(s);
+      //updateFunction(serve(this, "Happy Eating!", this.customer))
+    }, 2000)
+  }
+}
+
+cake.decorate(updateStatus);
+
+var pie = {
+  name: "Apple Pie",
+  ingredients: ["apples", "flour", "eggs", "butter", "sugar"],
+  topping: "streusel",
+  bakeTemp: "350 degrees",
+  bakeTime: "75 minutes",
+  customer: "Tammy"
+}
+
+function makeCake() {
+  var updateCakeStatus;
+  mix(updateCakeStatus)
+}
+
+function makePie() {
+  var updatePieStatus;
+  mix(updatePieStatus)
+}
+
+function updateStatus(statusText) {
+//console.log(this); this is cake
+  document.getElementsByClassName("status")[0].innerText = statusText
+}
+
+function bake(updateFunction) {
+  var status = "Baking at " + this.bakeTemp + " for " + this.bakeTime
+  setTimeout(function() {
+    cool(updateFunction)
+  }, 2000)
+}
+
+function mix(updateFunction) {
+  var status = "Mixing " + this.ingredients.join(", ")
+  setTimeout(function() {
+    bake(updateFunction)
+  }, 2000)
+  updateFunction(status)
+}
+
+function cool(updateFunction) {
+  var status = "It has to cool! Hands off!"
+  setTimeout(function() {
+    this.decorate(updateFunction)
+  }, 2000)
+}
+
+function makeDessert() {
+  //add code here to decide which make... function to call
+  //based on which link was clicked
+}
+
+function serve(message, customer) {
+  //you shouldn't need to alter this function
+console.log(this);
+    return(customer + ", your " + this.name + " is ready to eat! " + message)
+}
